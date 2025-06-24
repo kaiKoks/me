6b291aeefe05ab2446588c6476104da81cfc7a8f
kai <cheremisin.ilya@gmail.com>
2025-06-24 19:18:13 +0300

    theme contxt init

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from "react"
+
+`use client`
+
+type Theme = 'light' | 'dark'
+
+interface ThemeContexType {
+    theme: Theme,
+    toggleTheme: () => void
+}
+const ThemeContex = createContext<ThemeContexType | undefined>(undefined)
+
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [theme, setTheme] = useState<Theme>('dark')
+    const toggleTheme = () => {
+        theme === 'dark' ? setTheme('light') : setTheme('dark')
+    }
+    return (
+        <ThemeContex.Provider value={{ theme, toggleTheme }}>{children}</ThemeContex.Provider>
+    )
+}
