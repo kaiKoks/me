ebb606f2754853acd40c0fcb8bcd510cfc979eda
kai <cheremisin.ilya@gmail.com>
2025-06-30 16:33:08 +0300

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      hi
+    <div className="font-[family-name:var(--font-geist-sans)] flex flex-col items-center p-10 text-4xl">
+        hi
     </div>
   );
 }
