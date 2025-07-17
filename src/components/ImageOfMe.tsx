89cc89f318fa24e5735a4c2756f4334616501cef
kai <cheremisin.ilya@gmail.com>
2025-07-17 14:32:09 +0300

    image styles

--- src/components/ImageOfMe.tsx
+++ src/components/ImageOfMe.tsx
@@ -5,10 +5,8 @@ export default function ImageOfMe() {
     const [isLoaded, setIsLoaded] = useState(false);
 
     return (
-        <div className="w-40 h-40 shrink-0 relative rounded-full bg-neutral-200 dark:bg-neutral-800 flex items-center justify-center">
-            {!isLoaded && (
-                <div className='w-12 h-12 border-dotted rounded-full border-8 border-[#1d4ed8]/40 border-t-transparent animate-[spin_2s_linear_infinite]'></div>
-            )}
-            <Image src='/me.png' sizes="160" fill alt="me" priority={true} onLoadingComplete={() => setIsLoaded(true)} className={`${isLoaded ? "opacity-100" : "opacity-0"} rounded-full object-cover brightness-120 contrast-95 object-[50%_5%] shadow-xl ring-2 ring-gray-800 dark:ring-gray-300 text-center transition-opacity duration-250`} ></Image>
+        <div className="w-40 h-40 shrink-0 relative rounded-full bg-neutral-200 dark:bg-neutral-800 flex items-center justify-center ring-2 ring-gray-800 dark:ring-gray-300 ">
+        
+            <Image src='/me.png' sizes="160" fill alt="me" priority={true} onLoadingComplete={() => setIsLoaded(true)} className={`${isLoaded ? "opacity-100" : "opacity-0"} rounded-full object-cover brightness-120 contrast-95 object-[50%_5%] shadow-xl text-center transition-opacity duration-100`} ></Image>
         </div>)
 }
