82113f0548dcdd28f4ec3b04d68a31f1cee9e489
kai <cheremisin.ilya@gmail.com>
2025-09-25 10:22:24 +0300

    fixed image src

--- src/components/pageContent/ImageOfMe.tsx
+++ src/components/pageContent/ImageOfMe.tsx
@@ -7,7 +7,7 @@ export default function ImageOfMe() {
   return (
     <div className="w-40 h-40 shrink-0 relative rounded-full bg-neutral-200 dark:bg-neutral-800 flex items-center justify-center ring-2 ring-gray-800 dark:ring-gray-300 ">
       <Image
-        src="/me.png"
+        src="/images/me.png"
         sizes="160"
         fill
         alt="me"
